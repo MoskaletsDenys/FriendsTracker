31c31f7c6ab1a382fe0f338dcfeab588c59ea170
Moskalets Denys <moskaletsdenys@gmail.com>
2020-09-01 16:52:04 +0300

    Entity Framework added CRUD operations

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,32 +6,87 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using FriendsTracker.Models;
+//???
+using Microsoft.EntityFrameworkCore;
 
 namespace FriendsTracker.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private ApplicationContext db;
+        public HomeController(ApplicationContext context)
         {
-            _logger = logger;
+            db = context;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Friends.ToListAsync());
+        }
+        public IActionResult Create()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> Create(Friend friend)
         {
-            return View();
+            db.Friends.Add(friend);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id != null)
+            {
+                Friend friend = await db.Friends.FirstOrDefaultAsync(p => p.Id == id);
+                if (friend != null)
+                    return View(friend);
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Friend friend = await db.Friends.FirstOrDefaultAsync(p => p.Id == id);
+                if (friend != null)
+                    return View(friend);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Friend friend)
+        {
+            db.Friends.Update(friend);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        [ActionName("Delete")]
+        public async Task<IActionResult> ConfirmDelete(int? id)
+        {
+            if (id != null)
+            {
+                Friend friend = await db.Friends.FirstOrDefaultAsync(p => p.Id == id);
+                if (friend != null)
+                    return View(friend);
+            }
+            return NotFound();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            if (id != null)
+            {
+                Friend friend = await db.Friends.FirstOrDefaultAsync(p => p.Id == id);
+                if (friend != null)
+                {
+                    db.Friends.Remove(friend);
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            return NotFound();
         }
     }
 }
---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendsTracker.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Friend> Friends { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
