a1815b8a461f6a6c9d43fcd59adb5ed2a926c400
Alex.Kovalskyi <alex.kowalskiy@gmail.com>
2020-09-06 19:16:22 +0300

    added TODO items

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// TODO Move file to project root
 namespace FriendsTracker.Models
 {
     public class ApplicationContext:DbContext
